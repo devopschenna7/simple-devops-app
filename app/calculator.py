4252d4c52714814dc0127c136cb18ed9b35afb1f
devopschenna7 <devopschenna7@gmail.com>
2026-04-19 00:34:04 +0530

    updated

--- app/calculator.py
+++ app/calculator.py
@@ -1,10 +1,2 @@
-from app.calculator import add
-
-
-def test_add():
-    assert add(2, 3) == 5
-
-
-def test_add_negative():
-    assert add(-1, -1) == -2
-
+def add(a, b):
+    return a + b
--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -1,7 +1,10 @@
 from app.calculator import add
 
+
 def test_add():
     assert add(2, 3) == 5
 
+
 def test_add_negative():
     assert add(-1, -1) == -2
+
