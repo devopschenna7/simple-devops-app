56ef19e823de7bb4dce385961cf13ee5503c0091
chennaraodevops7 <chennaraodevops7@gmail.com>
2026-08-09 10:03:56 +0530

    updated

--- app/app.py
+++ app/app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return f"CI/CD Demo 🚀 10 + 20 = {add(10,20)}"
+    return f"CI/CD Demo 🚀 11 + 20 = {add(11,20)}"
 
 
 if __name__ == "__main__":
